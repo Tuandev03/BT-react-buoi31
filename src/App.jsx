687100a30c71ac0d019037c00692c24127db031b
Thanh Tuan <you@example.com>
2024-03-20 10:42:02 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import GlassesModel from "./components/GlassesModel";
+const App = () => {
+  const data = [
+    {
+      id: 1,
+      price: 30,
+      name: "GUCCI G8850U",
+      url: "../public/g1.jpg",
+      image: "../public/v1.png",
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip 1. ",
+    },
+    {
+      id: 2,
+      price: 50,
+      name: "GUCCI G8759H",
+      url: "../public/g2.jpg",
+      image: "../public/v2.png",
+
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip 2. ",
+    },
+    {
+      id: 3,
+      price: 30,
+      name: "DIOR D6700HQ",
+      url: "../public/g3.jpg",
+      image: "../public/v3.png",
+
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip 3. ",
+    },
+    {
+      id: 4,
+      price: 70,
+      name: "DIOR D6005U",
+      url: "../public/g4.jpg",
+      image: "../public/v4.png",
+
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip 4. ",
+    },
+    {
+      id: 5,
+      price: 40,
+      name: "PRADA P8750",
+      url: "../public/g5.jpg",
+      image: "../public/v5.png",
+
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip 5. ",
+    },
+    {
+      id: 6,
+      price: 60,
+      name: "PRADA P9700",
+      url: "../public/g6.jpg",
+      image: "../public/v6.png",
+
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip 6. ",
+    },
+    {
+      id: 7,
+      price: 80,
+      name: "FENDI F8750",
+      url: "../public/g7.jpg",
+      image: "../public/v7.png",
+
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip 7. ",
+    },
+    {
+      id: 8,
+      price: 100,
+      name: "FENDI F8500",
+      url: "../public/g8.jpg",
+      image: "../public/v8.png",
+
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip 8. ",
+    },
+    {
+      id: 9,
+      price: 60,
+      name: "FENDI F4300",
+      url: "../public/g9.jpg",
+      image: "../public/v9.png",
+
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    },
+  ];
+
+  return (
+    <div>
+      <GlassesModel data={data}></GlassesModel>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/GlassesModel.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+function GlassesModel({ data }) {
+  const [selectedGlasses, setSelectedGlasses] = useState(null);
+
+  const handleGlassesClick = (item) => {
+    setSelectedGlasses(item);
+  };
+
+  return (
+    <div className="  container-fluid mx-auto bg-blue-200 flex justify-center gap-10   h-lvh">
+      <div className="flex flex-col justify-center items-center gap-5">
+        <div className="relative">
+          <img className="  " src="../public/model.jpg" alt="" />
+          {selectedGlasses && (
+            <div className=" p-3">
+              <img
+                className="w-[50%] absolute top-[25%] left-[122px] max-sm:top-[25%]  max-sm:left-[106px] "
+                src={selectedGlasses.image}
+                alt={selectedGlasses.name}
+              />
+              <div className=" ">
+                <p className="absolute bottom-36 text-3xl font-semibold">
+                  {selectedGlasses.name}
+                </p>
+                <p className="absolute bottom-14 text-xl text-gray-600">
+                  {selectedGlasses.desc}
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="mx-auto bg-white  ">
+          <div className="flex items-center flex-wrap">
+            {data.map((item, index) => (
+              <div
+                className="w-1/4 p-2 flex justify-center items-center"
+                key={item.id}
+                onClick={() => handleGlassesClick(item)}
+              >
+                <img
+                  className="w-[70px] cursor-pointer"
+                  src={item.url}
+                  alt={item.name}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default GlassesModel;
